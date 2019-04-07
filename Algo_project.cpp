3dd44548e4391c88cb556cd7b4b6f67d34dc8fc8
ankitabansal123 <47985955+ankitabansal123@users.noreply.github.com>
2019-04-07 12:39:57 +0530

    Add files via upload

---
+++ Algo_project.cpp
@@ -0,0 +1,37 @@
+# include<iostream>
+# include<bits/stdc++.h>
+# include<string.h>
+# include<string>
+using namespace std;
+void clrscr()
+{
+   system("cls");
+}
+class Registration
+{
+    public:
+    int client_no;
+    string Fname;
+    string Lname;
+    string date;
+    string address;
+    int mob_no;
+    float cost;
+
+    void Reg()
+    {
+        cin>>client_no>>Fname>>Lname>>date>>address>>mob_no>>cost;
+        clrscr();
+    }
+};
+int main()
+{
+    Registration r;
+    cout<<"Client_no.     Fname       Lname       date                  address                 mob_no          cost"<<endl;
+    cout<<"-------------------------------------------------------------------------------------------------------------------"<<endl;
+    r.Reg();
+    while(true)
+    {
+        r.Reg();
+    }
+}
